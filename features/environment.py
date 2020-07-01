c2c2505223554885ef849369986e77afed5a680c
bartlomiej-lazarek <bartlomiej.lazarek@gmail.com>
2020-07-01 20:20:46 +0200

    Add config.ini to gitignore file and before_tag hook to enviornment

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 
-
-def before_scenario(context, feature):
-    context.driver = webdriver.Chrome()
-    context.driver.maximize_window()
-    context.driver.implicitly_wait(5)
+from pages.base_page import BasePage
 
 
 def after_scenario(context, feature):
     context.driver.quit()
+
+
+def before_tag(context, tag):
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(5)
+
+    if tag.startswith("home"):
+        context.page = BasePage(context.driver)
